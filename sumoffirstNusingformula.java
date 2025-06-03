cbb583647d7123cb1722c5e89707bbc734a40f46
prasanna kothoju <134475079+prasannakothoju@users.noreply.github.com>
2025-06-03 17:10:17 +0530

    Create sumoffirstNusingformula.java

---
+++ sumoffirstNusingformula.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class A{
+    
+    public static void sumof(int n){
+        int sum = n*(n+1)/2;
+        System.out.println("The sum is:"+sum);
+    }
+    
+    public static void main(String[] args){
+        sumof(6);
+    }
+}
